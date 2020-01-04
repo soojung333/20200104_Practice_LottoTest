2c323d28b5a25199db8ef1410f3d2711497df092
soojung <blessme333@gmail.com>
2020-01-04 11:00:22 +0900

    로또화면 초안까지 작성

--- app/src/main/java/com/soo/a20200104_practice_lottotest/LottoActivity.kt
+++ app/src/main/java/com/soo/a20200104_practice_lottotest/LottoActivity.kt
@@ -3,13 +3,15 @@ package com.soo.a20200104_practice_lottotest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+class LottoActivity : BaseActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lotto)
         setupEvents()
         setValues()
+
     }
 
 
@@ -20,6 +22,4 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
     }
-
-
 }
