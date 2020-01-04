32272c06886f441ec93f9aa86b4a5450e1fe818e
soojung <blessme333@gmail.com>
2020-01-04 11:57:44 +0900

    당첨번호 화면에 뿌리기

--- app/src/main/java/com/soo/a20200104_practice_lottotest/LottoActivity.kt
+++ app/src/main/java/com/soo/a20200104_practice_lottotest/LottoActivity.kt
@@ -3,6 +3,7 @@ package com.soo.a20200104_practice_lottotest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_lotto.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -11,6 +12,7 @@ import kotlin.random.Random
 class LottoActivity : BaseActivity() {
 
     val winLottoNumArr = ArrayList<Int>()
+    val winLottoNumTextViewList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,14 +70,30 @@ class LottoActivity : BaseActivity() {
 //        Collections 클래스의 기능을 이용해서 Arraylist 내부 값을 정렬 기능 이용.
         Collections.sort(winLottoNumArr)
 
-        for (num in winLottoNumArr) {
-            Log.d("당첨번호", num.toString())
+//       6개의 당첨번호 / 각 자리의 텍스트뷰를 매칭하는 for문
+
+        for (i in 0..5) {
+
+//            상황에 맞는 텍스트뷰 / 당첨번호를 뽑아와서
+            val tempTextView = winLottoNumTextViewList.get(i)
+            val winNum = winLottoNumArr.get(i)
+
+//            값으로 세팅
+            tempTextView.text = winNum.toString()
+
         }
 
     }
 
 
     override fun setValues() {
+
+        winLottoNumTextViewList.add(lottoNumTxt01)
+        winLottoNumTextViewList.add(lottoNumTxt02)
+        winLottoNumTextViewList.add(lottoNumTxt03)
+        winLottoNumTextViewList.add(lottoNumTxt04)
+        winLottoNumTextViewList.add(lottoNumTxt05)
+        winLottoNumTextViewList.add(lottoNumTxt06)
 
     }
 
